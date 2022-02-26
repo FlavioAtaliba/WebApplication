402f2947223a90e0d690d2bf2ecf20bb6156f817
0220481923007 <0220481923007@LAB4TC02>
2022-02-25 21:35:08 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication2/Controllers/ProdutoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication2/Models/ProdutoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class ProdutoModel
+    {
+        public int Codigo { get; set; }
+        public decimal Valor { get; set; }
+        public string Nome { get; set; }
+
+
+    }
+}
